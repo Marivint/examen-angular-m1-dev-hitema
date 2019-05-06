2f4a4c72f148c973f0cf393822aee36efdeca8b9
Unknown <marivint.yvann@gmail.com>
2019-05-06 10:11:08 +0200

    formater l'affichage des températures

---
+++ src/app/city-weather/number-format.pipe.ts
@@ -0,0 +1,21 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'numberFormat'
+// })
+// export class numberFormatPipe implements PipeTransform {
+//   transform(value: number): number {
+//   	// let res = parseFloat(value);
+//   	return Math.pow(value, isNaN(res) ? 1 : res);
+//   }
+// }
+
+
+
+
+// export class ExponentialStrengthPipe implements PipeTransform {
+//   transform(value: number, exponent: string): number {
+//     let exp = parseFloat(exponent);
+//     return Math.pow(value, isNaN(exp) ? 1 : exp);
+//   }
+// }
